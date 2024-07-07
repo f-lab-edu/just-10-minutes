db1daadc086802403e5ca7f06a5ed948ae592596
HONGJIBEOM <corea1836@naver.com>
2024-07-07 10:32:46 +0900

    first commit : 프로젝트 세팅

---
+++ src/main/java/com/flab/just_10_minutes/Just10MinutesApplication.java
@@ -0,0 +1,13 @@
+package com.flab.just_10_minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Just10MinutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Just10MinutesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/just_10_minutes/Just10MinutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.just_10_minutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Just10MinutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
