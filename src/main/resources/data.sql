71e7ceba922de168bc2bb97c30970ee70624b26b
HONGJIBEOM <corea1836@naver.com>
2024-07-08 07:29:59 +0900

    chore: 테스트 용 sql 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO user_table(login_id, password, phone, address, role) VALUES ('existTestId', 'testPassword', '010-1234-5678', 'testAddress', 'PUBLIC')
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user_table (
+    id int AUTO_INCREMENT,
+    login_id VARCHAR(12),
+    password VARCHAR(15),
+    phone VARCHAR(13),
+    address VARCHAR(30),
+    role VARCHAR(20),
+    PRIMARY KEY(id)
+);
