014008bab5f65fe5c5b13c8ef2cc5ebd5ac13d9e
HONGJIBEOM <corea1836@naver.com>
2024-07-08 22:00:24 +0900

    feat: userController 생성
    - PUBLIC 권한 유저 생성 기능 개발

---
+++ src/main/java/com/flab/just_10_minutes/User/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.flab.just_10_minutes.User.controller;
+
+import com.flab.just_10_minutes.User.domain.User;
+import com.flab.just_10_minutes.User.dto.UserDto;
+import com.flab.just_10_minutes.User.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users/")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/public")
+    public ResponseEntity<HttpStatus> signUpPublic(@RequestBody @Valid UserDto userDto) {
+
+        userService.save(User.builder()
+                .loginId(userDto.getLoginId())
+                .password(userDto.getPassword())
+                .phone(userDto.getPhone())
+                .address(userDto.getAddress())
+                .role(User.ROLE.PUBLIC)
+                .build());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
