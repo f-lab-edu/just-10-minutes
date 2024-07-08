c8dfa78469a7df0432a85ba359af6d28e3bf9c9a
HONGJIBEOM <corea1836@naver.com>
2024-07-08 21:57:25 +0900

    test: userMapper 테스트

---
+++ src/test/java/com/flab/just_10_minutes/User/UserMapperTest.java
@@ -0,0 +1,66 @@
+package com.flab.just_10_minutes.User;
+
+import com.flab.just_10_minutes.User.domain.User;
+import com.flab.just_10_minutes.User.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@MybatisTest
+public class UserMapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Test
+    public void findByLoginId_유저가_없는_경우() {
+        //given
+        final String longinId = "notExitsTestId";
+
+        //when
+        User user = userMapper.findByLoginId(longinId);
+
+        //then
+        assertThat(user).isNull();
+    }
+
+    @Test
+    public void findByLoginId_유저가_존재하는_경우() {
+        //given
+        final String longinId = "existTestId";
+
+        //when
+        User user = userMapper.findByLoginId(longinId);
+
+        //then
+        assertThat(user.getLoginId()).isEqualTo("existTestId");
+        assertThat(user.getPassword()).isEqualTo("testPassword");
+        assertThat(user.getPhone()).isEqualTo("010-1234-5678");
+        assertThat(user.getAddress()).isEqualTo("testAddress");
+        assertThat(user.getRole()).isEqualTo(User.ROLE.PUBLIC);
+    }
+
+    @Test
+    public void save_성공() {
+
+        User user = User.builder()
+                .loginId("testId")
+                .password("testPassword")
+                .phone("010-1234-5678")
+                .address("testAddress")
+                .role(User.ROLE.PUBLIC)
+                .build();
+
+        //when
+        int saveResult = userMapper.save(user);
+        User saveUser = userMapper.findByLoginId(user.getLoginId());
+
+        //then
+        assertThat(saveResult).isEqualTo(1);
+        assertThat(saveUser.getId()).isEqualTo(2);
+    }
+
+}
