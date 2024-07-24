f2c89d69e83f6b140360d13a5428c09549fcdc76
HONGJIBEOM <corea1836@naver.com>
2024-07-24 23:27:42 +0900

    feat: showUserProfile 메서드 추가

--- src/main/java/com/flab/just_10_minutes/User/controller/UserController.java
+++ src/main/java/com/flab/just_10_minutes/User/controller/UserController.java
@@ -1,15 +1,14 @@
 package com.flab.just_10_minutes.User.controller;
 
+import com.flab.just_10_minutes.User.domain.User;
 import com.flab.just_10_minutes.User.dto.UserDto;
+import com.flab.just_10_minutes.User.dto.UserProfileDto;
 import com.flab.just_10_minutes.User.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,10 +17,22 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/sign-up")
+    @PostMapping("/sign_up")
     public ResponseEntity<HttpStatus> signUp(@RequestBody @Valid UserDto userDto) {
         userService.save(UserDto.toDomain(userDto));
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/profile/{loginId}")
+    public ResponseEntity<UserProfileDto> showUserProfile(@PathVariable final String loginId) {
+        User user = userService.getUserProfile(loginId);
+
+        return ResponseEntity.ok().body(UserProfileDto.builder()
+                .loginId(user.getLoginId())
+                .address(user.getAddress())
+                .phone(user.getPhone())
+                .role(user.getRole())
+                .build());
     }
 }
