d6867b2ffd4071670c914f7c66d5c0d8c657acb3
HONGJIBEOM <corea1836@naver.com>
2024-07-24 23:26:44 +0900

    feat: getUserProfile 메서드 추가

--- src/main/java/com/flab/just_10_minutes/User/service/UserService.java
+++ src/main/java/com/flab/just_10_minutes/User/service/UserService.java
@@ -1,11 +1,12 @@
 package com.flab.just_10_minutes.User.service;
 
 import com.flab.just_10_minutes.User.domain.User;
-import com.flab.just_10_minutes.User.persistence.UserDao;
-import com.flab.just_10_minutes.Util.ErrorResult.UserErrorResult;
-import com.flab.just_10_minutes.Util.Exception.UserException;
+import com.flab.just_10_minutes.User.infrastructure.UserDao;
+import com.flab.just_10_minutes.Util.Exception.Business.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.flab.just_10_minutes.Util.contants.ResponseMessage.DUPLICATED_REGISTER;
 
 @Service
 @RequiredArgsConstructor
@@ -14,13 +15,17 @@ public class UserService {
     private final UserDao userDao;
 
     public void save(final User user) {
-        if (validateExistedUser(user.getLoginId())) {
-            throw new UserException(UserErrorResult.DUPLICATED_USER_REGISTER);
+        if (existsByLoginId(user.getLoginId())) {
+            throw new BusinessException(DUPLICATED_REGISTER);
         }
         userDao.save(user);
     }
 
-    public Boolean validateExistedUser(final String loginId) {
-        return userDao.findByLoginId(loginId).isPresent();
+    public boolean existsByLoginId(final String loginId) {
+        return userDao.existsByLoginId(loginId);
+    }
+
+    public User getUserProfile(final String loginId) {
+        return userDao.fetch(loginId);
     }
 }
