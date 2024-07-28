38e0ee19a0426dfc47cd6c875094d2ce21cc1374
HONGJIBEOM <corea1836@naver.com>
2024-07-28 21:16:08 +0900

    feat: PointController 리팩토링
    - Point 도메인 통합 테스트 추가

--- src/main/java/com/flab/just_10_minutes/Point/controller/PointController.java
+++ src/main/java/com/flab/just_10_minutes/Point/controller/PointController.java
@@ -1,6 +1,8 @@
 package com.flab.just_10_minutes.Point.controller;
 
+import com.flab.just_10_minutes.Point.domain.PointHistory;
 import com.flab.just_10_minutes.Point.dto.PointHistoryDto;
+import com.flab.just_10_minutes.Point.dto.PointStatusDto;
 import com.flab.just_10_minutes.Point.service.PointService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -8,18 +10,31 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/points")
 public class PointController {
 
-    private PointService pointService;
+    private final PointService pointService;
 
-    @PostMapping
-    public ResponseEntity<HttpStatus> offerPoint(@RequestBody @Valid PointHistoryDto pointHistoryDto) {
+    @PostMapping("/offer")
+    public ResponseEntity<PointHistory> offerPoint(@RequestBody @Valid PointHistoryDto pointHistoryDto) {
         //TODO : ADMIN 권한을 가진 유저만 실행할 수 있도록 수정. due date : 7/16
-        pointService.save(PointHistoryDto.toDomain(pointHistoryDto));
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        PointHistory latestHistory = pointService.offerPoint(PointHistoryDto.toDomain(pointHistoryDto));
+
+        return ResponseEntity.ok(latestHistory);
+    }
+
+    @GetMapping("/total/{loginId}")
+    public ResponseEntity<Long> showTotal(@PathVariable final String loginId) {
+        return ResponseEntity.ok(pointService.getTotalPoint(loginId));
+    }
+
+    @GetMapping("/histories/{loginId}")
+    public ResponseEntity<PointStatusDto> showHistories(@PathVariable final String loginId) {
+        return ResponseEntity.ok(pointService.getPointHistories(loginId));
     }
 }
