05aa83f35921f550292e5e38d0fefd8626c0ace4
HONGJIBEOM <corea1836@naver.com>
2024-07-28 20:56:47 +0900

    chore: DB 설정 추가 : Mybatis 카멜 케이스

---
+++ src/test/java/com/flab/just_10_minutes/Point/PointHistoryTestFixture.java
@@ -0,0 +1,36 @@
+package com.flab.just_10_minutes.Point;
+
+import com.flab.just_10_minutes.Point.domain.PointHistory;
+
+import static com.flab.just_10_minutes.User.UserDtoTestFixture.EXIST_ID;
+
+
+public class PointHistoryTestFixture {
+
+    public static PointHistory createPointHistory(Long quantity) {
+        return PointHistory.builder()
+                .loginId(EXIST_ID)
+                .quantity(quantity)
+                .reason("test")
+                .totalQuantity(0L)
+                .build();
+    }
+
+    public static PointHistory createPointHistory(String loginId, Long quantity) {
+        return PointHistory.builder()
+                .loginId(loginId)
+                .quantity(quantity)
+                .reason("test")
+                .totalQuantity(0L)
+                .build();
+    }
+
+    public static PointHistory createPointHistory(String loginId, Long quantity, String reason) {
+        return PointHistory.builder()
+                .loginId(loginId)
+                .quantity(quantity)
+                .reason(reason)
+                .totalQuantity(0L)
+                .build();
+    }
+}
