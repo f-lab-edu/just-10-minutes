d235f241a32d3aed53d256e6e9907b74ab8be4f3
HONGJIBEOM <corea1836@naver.com>
2024-08-11 02:32:39 +0900

    refactor: 예외 메세지 수정
    ##NotFoundException
    - Not Found #{target} 타겟을 받아 메세지 반환 하도록 수
    ##etc
    - 글로벌 메세지 추가

--- src/main/java/com/flab/just_10_minutes/Util/Exception/Database/DuplicatedKeyException.java
+++ src/main/java/com/flab/just_10_minutes/Util/Exception/Database/DuplicatedKeyException.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class DuplicatedKeyException extends DatabaseException{
 
-    public static final String DUPLICATED_KEY_MSG = "Duplicated Key Register";
+    public static final String DUPLICATED_KEY = "Duplicated Key Error";
 
     public DuplicatedKeyException(String message) {
         super(message, HttpStatus.CONFLICT);
--- src/main/java/com/flab/just_10_minutes/Util/Exception/Database/InternalException.java
+++ src/main/java/com/flab/just_10_minutes/Util/Exception/Database/InternalException.java
@@ -6,6 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class InternalException extends DatabaseException{
 
+    public static final String FAIL_TO_INSERT = "Fail To Insert Error";
+    public static final String FAIL_TO_UPDATE = "Fail To Update Error";
+    public static final String FAIL_TO_DELETE = "Fail To Delete Error";
+
     public InternalException(String message) {
         super(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
