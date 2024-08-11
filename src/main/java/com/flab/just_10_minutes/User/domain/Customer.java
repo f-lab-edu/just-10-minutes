9fb7b3ffff0042574bc7eb8d20e32f9fda672e30
HONGJIBEOM <corea1836@naver.com>
2024-08-11 15:56:28 +0900

    feat: Product, Seller 클래스 분리

---
+++ src/main/java/com/flab/just_10_minutes/User/domain/Customer.java
@@ -0,0 +1,24 @@
+package com.flab.just_10_minutes.User.domain;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@EqualsAndHashCode
+public class Customer {
+
+    private String loginId;
+    private String phone;
+    private String address;
+    private User.ROLE role;
+
+    public static Customer from(User user) {
+        return Customer.builder()
+                .loginId(user.getLoginId())
+                .phone(user.getPhone())
+                .address(user.getAddress())
+                .role(user.getRole())
+                .build();
+    }
+}
