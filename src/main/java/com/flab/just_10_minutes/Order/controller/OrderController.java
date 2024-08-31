f768dfdc0c5ce5dbdf436c4932ad7cd7279a4ad9
HONGJIBEOM <corea1836@naver.com>
2024-08-31 16:30:35 +0900

    refactor: IamportException 제거

--- src/main/java/com/flab/just_10_minutes/Order/controller/OrderController.java
+++ src/main/java/com/flab/just_10_minutes/Order/controller/OrderController.java
@@ -3,7 +3,6 @@ package com.flab.just_10_minutes.Order.controller;
 import com.flab.just_10_minutes.Order.dto.OrderDto;
 import com.flab.just_10_minutes.Order.dto.OrderReceiptDto;
 import com.flab.just_10_minutes.Order.service.OrderService;
-import com.siot.IamportRestClient.exception.IamportResponseException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,7 +19,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<OrderReceiptDto> order(@Valid @RequestBody OrderDto orderDto) throws IamportResponseException, IOException {
+    public ResponseEntity<OrderReceiptDto> order(@Valid @RequestBody OrderDto orderDto) {
         return ResponseEntity.ok(orderService.order(orderDto));
     }
 }
