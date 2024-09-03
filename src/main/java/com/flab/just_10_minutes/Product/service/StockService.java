9da73b185f857a88412a92e5dbc5e9da99d42b97
HONGJIBEOM <corea1836@naver.com>
2024-09-04 05:51:43 +0900

    refactor: 동시성 확인 로그 추가

--- src/main/java/com/flab/just_10_minutes/Product/service/StockService.java
+++ src/main/java/com/flab/just_10_minutes/Product/service/StockService.java
@@ -4,23 +4,30 @@ import com.flab.just_10_minutes.Product.domain.Product;
 import com.flab.just_10_minutes.Product.infrastructure.repository.ProductDao;
 import com.flab.just_10_minutes.Util.Exception.Business.BusinessException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StockService {
 
     private final ProductDao productDao;
 
-    @Transactional
+    @Transactional(timeout = 100)
     public Product decreaseStock(final Long productId, final Long requestQuantity) {
+        log.info("[Thread id] " + Thread.currentThread().getId() + " Transaction Start");
         Product product = productDao.fetchWithPessimisticLock(productId);
+        log.info("[Product id] " + product.getId() + " Current Stock : " + product.getPurchasedStock());
         Long updatedStock = Math.abs(requestQuantity) + product.getPurchasedStock();
 
         if (updatedStock > product.getTotalStock()) {
             throw new BusinessException("Exceed Total Stock");
         }
-        return productDao.patchStock(productId, updatedStock);
+        Product newProduct = productDao.patchStock(productId, updatedStock);
+        log.info("[Product id]" + newProduct.getId() + " CurrentStock :" + newProduct.getPurchasedStock());
+        log.info("[Thread id] " + Thread.currentThread().getId() + " Transaction End");
+        return newProduct;
     }
 }
