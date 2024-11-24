d93ce122762265964a13c06c780016430d0c9e22
HONGJIBEOM <corea1836@naver.com>
2024-11-24 19:30:26 +0900

    feat: fcm 전송 실패 시 재전송 로직 추가(5XX대에 재시도)

--- src/main/java/com/flab/just_10_minutes/notification/service/eventListener/PublishFcmNotificationEventListener.java
+++ src/main/java/com/flab/just_10_minutes/notification/service/eventListener/PublishFcmNotificationEventListener.java
@@ -8,7 +8,10 @@ import com.flab.just_10_minutes.notification.infrastructure.repository.FcmNotifi
 import com.flab.just_10_minutes.notification.infrastructure.repository.FcmTokenDao;
 import com.flab.just_10_minutes.util.exception.business.BusinessException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -16,9 +19,11 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.web.client.HttpServerErrorException;
 
 import static com.flab.just_10_minutes.util.executor.AsyncConfig.EVENT_HANDLER_TASK_EXECUTOR;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class PublishFcmNotificationEventListener {
@@ -32,29 +37,44 @@ public class PublishFcmNotificationEventListener {
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     @Retryable(
-            value = {BusinessException.class},
+            value = {HttpServerErrorException.class},
             maxAttempts = 3,
             backoff = @Backoff(delay = 1000, multiplier = 2)
     )
-    public void handleFcmChannelEvent(FcmNotificationEvent event) {
+    public void handleFcmNotificationEvent(FcmNotificationEvent event) {
         FcmToken fcmToken = fcmTokenDao.fetchByLoginId(event.getReceiverId());
         Campaign campaign = fcmCampaignDao.fetchById(event.getCampaignId());
         FcmNotification fcmNotification = FcmNotification.from(event, fcmToken);
 
         FcmApiV1Response fcmApiV1Response = fcmApiClient.sendMessage(fcmNotification, campaign);
-        if (fcmApiV1Response.getCode() != 200) {
 
+        handelFcmResponse(fcmApiV1Response, fcmNotification);
+    }
+
+    private void handelFcmResponse(FcmApiV1Response fcmApiV1Response, FcmNotification fcmNotification) {
             /*
-            401 : Google Credential error
+            401 : UNAUTHENTICATED(Google Credential error)
             400 : invalid argument
             403 : permission denied
+            404 : NOT FOUND(token not found)
             429 : quota exceeded(메시지 대상에 대한 전송 제한 초과)
             504 : unavailable(서버 과부하)
             500 : internal
              */
-            throw new BusinessException("Failed to reason : " + fcmApiV1Response.getMessage());
+        switch (fcmApiV1Response.getCode() / 100) {
+            case 2:
+                notificationDao.save(fcmNotification);
+                break;
+            case 5:
+                throw new HttpServerErrorException(HttpStatus.valueOf(fcmApiV1Response.getCode()), fcmApiV1Response.getMessage());
+            default:
+                throw new BusinessException("Failed to send FCM notification. Cause: " + fcmApiV1Response.getMessage());
         }
+    }
 
-        notificationDao.save(fcmNotification.update(true));
+    @Recover
+    public void recover(HttpServerErrorException e, FcmNotificationEvent event) {
+        log.error("Failed to send FCM notification after 3 times retries. Event: {}, Error: {}", event, e.getMessage());
+        throw new BusinessException("Failed after retries: " + e.getMessage());
     }
 }
--- src/main/java/com/flab/just_10_minutes/util/iamport/IamportConfig.java
+++ src/main/java/com/flab/just_10_minutes/util/iamport/IamportConfig.java
@@ -7,7 +7,6 @@ import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
