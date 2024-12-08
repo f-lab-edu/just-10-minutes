7a94462aac6e8d00f552c5852407a5dc2caab21e
HONGJIBEOM <corea1836@naver.com>
2024-12-08 23:03:07 +0900

    feat: SQS send 실패시 로깅 추가

--- src/main/java/com/flab/just_10_minutes/notification/service/NotificationService.java
+++ src/main/java/com/flab/just_10_minutes/notification/service/NotificationService.java
@@ -1,5 +1,6 @@
 package com.flab.just_10_minutes.notification.service;
 
+import com.flab.just_10_minutes.common.exception.business.BusinessException;
 import com.flab.just_10_minutes.notification.domain.Campaign;
 import com.flab.just_10_minutes.notification.domain.FcmToken;
 import com.flab.just_10_minutes.notification.domain.FcmNotificationEvent;
@@ -8,6 +9,8 @@ import com.flab.just_10_minutes.notification.dto.FcmNotificationRequest;
 import com.flab.just_10_minutes.notification.dto.FcmTokenRequest;
 import com.flab.just_10_minutes.notification.infrastructure.repository.CampaignDao;
 import com.flab.just_10_minutes.notification.infrastructure.repository.FcmTokenDao;
+import io.awspring.cloud.sqs.operations.MessagingOperationFailedException;
+import io.awspring.cloud.sqs.operations.SendResult;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -48,14 +51,22 @@ public class NotificationService {
         FcmToken fcmToken = fcmTokenDao.fetchByLoginId(fcmNotificationRequest.getLoginId());
         Campaign campaign = campaignDao.fetchById(fcmNotificationRequest.getCampaignId());
 
-        sqsTemplate.send(to -> {
-            to.queue(notificationQueueFIFO);
-            to.messageDeduplicationId(issueUUID().toString());
-            //TODO: MessageGroupId 설정
-            to.payload(FcmNotificationEvent.from(issueEventId(),
-                                                fcmToken.getLoginId(),
-                                                campaign.getId()
-            ));
-        });
+        send(FcmNotificationEvent.from(issueEventId(),
+                        fcmToken.getLoginId(),
+                        campaign.getId()));
+    }
+
+    private void send(FcmNotificationEvent event) {
+        try {
+            SendResult<Object> sendResult = sqsTemplate.send(to -> {
+                to.queue(notificationQueueFIFO);
+                to.messageDeduplicationId(issueUUID().toString());
+                //TODO: MessageGroupId 설정
+                to.payload(event);
+            });
+        } catch (MessagingOperationFailedException moe) {
+            log.error("Fail to send SQS. endpoint: {}, msg: {}, cause: {}", moe.getEndpoint(), moe.getMessage(), moe.getCause().getMessage());
+            throw new BusinessException("Fail to send Notification. message: " + moe.getMessage());
+        }
     }
 }
